74dd2144e08ba8e42e0fdd439d1aa5f08b595130
starkshn <starkshn97@gmail.com>
2023-02-26 23:54:57 +0900

    Affine Space - 선형성, 선형변환 => 도구가 "행렬" - 전단 변환 -> 2x2 정방행렬은 이동 안된다. => 차원을 늘린다. => "이동변환 행렬" - 1로 한정한 부분공간을 "아핀 공간"이라고한다. - 한 차원 높여 설계한 선형변환을 "아핀변환"이라한다. - 아핀공간의 벡터를 이동벡터 || 변위벡터라한다. - Affine Combination을 통해 새로운 점 생성가능 -> 직선, 반직선(Ray), 선분

---
+++ gamemath-6-1/gamemath-6-1/Source/Player/SoftRenderer2D.cpp
@@ -0,0 +1,169 @@
+
+#include "Precompiled.h"
+#include "SoftRenderer.h"
+#include <random>
+using namespace CK::DD;
+
+// 격자를 그리는 함수
+void SoftRenderer::DrawGizmo2D()
+{
+	auto& r = GetRenderer();
+	const auto& g = Get2DGameEngine();
+
+	// 그리드 색상
+	LinearColor gridColor(LinearColor(0.8f, 0.8f, 0.8f, 0.3f));
+
+	// 뷰의 영역 계산
+	Vector2 viewPos = g.GetMainCamera().GetTransform().GetPosition();
+	Vector2 extent = Vector2(_ScreenSize.X * 0.5f, _ScreenSize.Y * 0.5f);
+
+	// 좌측 하단에서부터 격자 그리기
+	int xGridCount = _ScreenSize.X / _Grid2DUnit;
+	int yGridCount = _ScreenSize.Y / _Grid2DUnit;
+
+	// 그리드가 시작되는 좌하단 좌표 값 계산
+	Vector2 minPos = viewPos - extent;
+	Vector2 minGridPos = Vector2(ceilf(minPos.X / (float)_Grid2DUnit), ceilf(minPos.Y / (float)_Grid2DUnit)) * (float)_Grid2DUnit;
+	ScreenPoint gridBottomLeft = ScreenPoint::ToScreenCoordinate(_ScreenSize, minGridPos - viewPos);
+
+	for (int ix = 0; ix < xGridCount; ++ix)
+	{
+		r.DrawFullVerticalLine(gridBottomLeft.X + ix * _Grid2DUnit, gridColor);
+	}
+
+	for (int iy = 0; iy < yGridCount; ++iy)
+	{
+		r.DrawFullHorizontalLine(gridBottomLeft.Y - iy * _Grid2DUnit, gridColor);
+	}
+
+	ScreenPoint worldOrigin = ScreenPoint::ToScreenCoordinate(_ScreenSize, -viewPos);
+	r.DrawFullHorizontalLine(worldOrigin.Y, LinearColor::Red);
+	r.DrawFullVerticalLine(worldOrigin.X, LinearColor::Green);
+}
+
+// 게임 오브젝트 목록
+
+
+// 최초 씬 로딩을 담당하는 함수
+void SoftRenderer::LoadScene2D()
+{
+	// 최초 씬 로딩에서 사용하는 모듈 내 주요 레퍼런스
+	auto& g = Get2DGameEngine();
+
+}
+
+// 게임 로직과 렌더링 로직이 공유하는 변수
+Vector2 currentPosition;
+float currentScale = 10.f;
+float currentDegree = 0.f;
+
+// 게임 로직을 담당하는 함수
+void SoftRenderer::Update2D(float InDeltaSeconds)
+{
+	// 게임 로직에서 사용하는 모듈 내 주요 레퍼런스
+	auto& g = Get2DGameEngine();
+	const InputManager& input = g.GetInputManager();
+
+	// 게임 로직의 로컬 변수
+	static float moveSpeed = 100.f;
+	static float scaleMin = 5.f;
+	static float scaleMax = 20.f;
+	static float scaleSpeed = 20.f;
+	static float rotateSpeed = 180.f;
+
+	Vector2 inputVector = Vector2(input.GetAxis(InputAxis::XAxis), input.GetAxis(InputAxis::YAxis)).GetNormalize();
+	Vector2 deltaPosition = inputVector * moveSpeed * InDeltaSeconds;
+	float deltaScale = input.GetAxis(InputAxis::ZAxis) * scaleSpeed * InDeltaSeconds;
+	float deltaDegree = input.GetAxis(InputAxis::WAxis) * rotateSpeed * InDeltaSeconds;
+
+	// 물체의 최종 상태 설정
+	currentPosition += deltaPosition;
+	currentScale = Math::Clamp(currentScale + deltaScale, scaleMin, scaleMax);
+	currentDegree += deltaDegree;
+}
+
+// 렌더링 로직을 담당하는 함수
+void SoftRenderer::Render2D()
+{
+	// 렌더링 로직에서 사용하는 모듈 내 주요 레퍼런스
+	auto& r = GetRenderer();
+	const auto& g = Get2DGameEngine();
+
+	// 배경에 격자 그리기
+	DrawGizmo2D();
+
+	// 렌더링 로직의 로컬 변수
+	float rad = 0.f;
+	static float increment = 0.001f;
+	static std::vector<Vector2> hearts;
+	HSVColor hsv(0.f, 1.f, 0.85f);
+
+	// 하트를 구성하는 점 생성
+	if (hearts.empty())
+	{
+		for (rad = 0.f; rad < Math::TwoPI; rad += increment)
+		{
+			float sin = sinf(rad);
+			float cos = cosf(rad);
+			float cos2 = cosf(2 * rad);
+			float cos3 = cosf(3 * rad);
+			float cos4 = cosf(4 * rad);
+			float x = 16.f * sin * sin * sin;
+			float y = 13 * cos - 5 * cos2 - 2 * cos3 - cos4;
+			hearts.push_back(Vector2(x, y));
+		}
+	}
+
+	// 아핀 변환행렬 (크기)
+	Vector3 sBasis1(currentScale, 0.f, 0.f);
+	Vector3 sBasis2(0.f, currentScale, 0.f);
+	Vector3 sBasis3 = Vector3::UnitZ;
+	Matrix3x3 sMatrix(sBasis1, sBasis2, sBasis3);
+	
+	// 아핀 변환행렬 (회전)
+	float sin = 0.f, cos = 0.f;
+	Math::GetSinCos(sin ,cos, currentDegree);
+	Vector3 rBasis1(cos, sin, 0.f);
+	Vector3 rBasis2(-sin, cos, 0.f);
+	Vector3 rBasis3 = Vector3::UnitZ;
+	Matrix3x3 rMatrix(rBasis1, rBasis2, rBasis3);
+
+	// 아핀 변환행렬 (이동)
+	Vector3 tBasis1 = Vector3::UnitX;
+	Vector3 tBasis2 = Vector3::UnitY;
+	Vector3 tBasis3(currentPosition.X, currentPosition.Y, 1.f);
+	Matrix3x3 tMatrix(tBasis1, tBasis2, tBasis3);
+	
+	// 모든 아핀 변환 행렬을 곱한 합성행렬. 크기 -> 회전 -> 이동 순으로 적용
+	Matrix3x3 finalMatrix = tMatrix * rMatrix * sMatrix;
+
+	// 각 값을 초기화 한 후 동일하게 증가시키면서 색상값을 지정
+	rad = 0.f;
+	for (auto const& v : hearts)
+	{
+		// 곱셈을 위해 하트를 구성하는 벡터를 3차원으로 변경
+		Vector3 newV(v.X, v.Y, 1.f);
+
+		// 아핀 변환을 적용하고 마지막 차원의 값을 제거하고 사용
+		Vector3 finalV = finalMatrix * newV;
+
+		hsv.H = rad / Math::TwoPI;
+		r.DrawPoint(finalV.ToVector2(), hsv.ToLinearColor());
+		rad += increment;
+	}
+
+	// 현재 위치, 크기, 각도를 화면에 출력
+	r.PushStatisticText(std::string("Position : ") + currentPosition.ToString());
+	r.PushStatisticText(std::string("Scale : ") + std::to_string(currentScale));
+	r.PushStatisticText(std::string("Degree : ") + std::to_string(currentDegree));
+}
+
+// 메시를 그리는 함수
+void SoftRenderer::DrawMesh2D(const class DD::Mesh& InMesh, const Matrix3x3& InMatrix, const LinearColor& InColor)
+{
+}
+
+// 삼각형을 그리는 함수
+void SoftRenderer::DrawTriangle2D(std::vector<DD::Vertex2D>& InVertices, const LinearColor& InColor, FillMode InFillMode)
+{
+}
